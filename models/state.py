1bc14c0785fad2b6152d3746349c8f487c91c6e7
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-02-13 15:15:49 +0200

    AirBnB

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represents a class that inherits from BaseModel
+        Public class attributes:
+           name: string - empty string
+    """
+
+    name = ""
